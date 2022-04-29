3b3daaa87c44d0ddea824bddd6a966a0b2692ee2
Ashik Reji <ashikreji27@gmail.com>
2022-04-30 03:22:43 +0400

    Completed step 1

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -2,24 +2,47 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  var id = 0
+  // const [id, setId] = useState(0);
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+   
+  const handleChange = (e) => {
+      console.log(e.target.value);
+      setNewName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    console.log("Clicked submit btn")
+    e.preventDefault();
+
+
+    const newPerson = {
+      name: newName
+    };
+
+    setPersons(persons.concat(newPerson));
+    console.log("persons array is now",persons)
+    setNewName("");
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input value={newName}  onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {/* persons.length > 1 && */}
+      {persons.map((person) => <p key={id++}>{person.name}</p>)}
     </div>
   );
 }
