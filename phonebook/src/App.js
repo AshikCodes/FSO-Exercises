668769eebcbf9e78991f4082413fb1ea5eb59335
Ashik Reji <ashikreji27@gmail.com>
2022-04-30 22:38:55 +0400

    Step 6 completed

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -3,16 +3,27 @@ import { useEffect, useState } from 'react';
 import UserForm from './components/UserForm';
 import SearchField from './components/SearchField';
 import UserList from './components/UserList';
+import axios from 'axios';
 
 function App() {
 
   const [filtered, setFiltered] = useState([]);
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNum: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', phoneNum: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', phoneNum: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', phoneNum: '39-23-6423122', id: 4 }
-  ])
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', phoneNum: '040-123456', id: 1 },
+  //   { name: 'Ada Lovelace', phoneNum: '39-44-5323523', id: 2 },
+  //   { name: 'Dan Abramov', phoneNum: '12-43-234345', id: 3 },
+  //   { name: 'Mary Poppendieck', phoneNum: '39-23-6423122', id: 4 }
+  // ]);
+  const [persons, setPersons] = useState([])
+
+  useEffect(() => {
+    axios 
+      .get('http://localhost:3001/persons')
+        .then((response) => {
+          const personsData = response.data;
+          setPersons(personsData);
+        })
+  },[])
 
   return (
     <div>
--- phonebook/src/components/UserList.js
+++ phonebook/src/components/UserList.js
@@ -2,7 +2,7 @@ const UserList = ({filtered, persons}) => {
     return ( 
         <div className="numberList">
             <h2>Numbers</h2>
-                {filtered.length > 0 ? filtered.map((person) => <p key={person.name}>{person.name} ---- {person.phoneNum}</p>) : persons.map((person) => <p key={person.name}>{person.name} ---- {person.phoneNum}</p>)}
+                {filtered.length > 0 ? filtered.map((person) => <p key={person.name}>{person.name} ---- {person.number}</p>) : persons.map((person) => <p key={person.name}>{person.name} ---- {person.phoneNum}</p>)}
         </div>
      );
 }
